f63d44e3cae350bf0e37a7e3468c83e815de1c6e
Alex HKD <hkdalex@gmail.com>
2021-06-19 12:38:21 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftRichString",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_14),
         .iOS(.v13),
         .tvOS(.v9),
         .watchOS(.v2)
